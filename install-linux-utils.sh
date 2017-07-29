e732a9a687665b12011976b57876218811d1e5f2
Bryan <bryan@bluenestdigital.com>
2017-07-28 22:20:04 -0400

    Fix chmod

--- install-linux-utils.sh
+++ install-linux-utils.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir /usr/local/bin/bnd
 cp -R . /usr/local/bin/bnd
-chmod 775 -R /usr/local/bin/bnd
+chmod -R 775 /usr/local/bin/bnd
 
 ln -s /usr/local/bin/bnd/replace-text.sh /usr/local/bin/replacetext
