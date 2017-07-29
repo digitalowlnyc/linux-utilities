e20b84f6b6147f15b0c5c61753982fca7a9cbb41
Bryan <bryan@bluenestdigital.com>
2017-07-28 21:40:23 -0400

    Add replace text util

---
+++ replace-text.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys, os, fileinput
+
+if len(sys.argv) < 3:
+	print("Missing argument: {file} {search} {replace}")
+	sys.exit()
+
+fileTarget = sys.argv[1]
+replaceText = sys.argv[2]
+withText = sys.argv[3]
+
+count = 0
+for line in fileinput.FileInput(fileTarget, inplace=1):
+    if replaceText in line:
+         count += 1
+         line = line.replace(replaceText, withText)
+
+print("Replaced %s lines in file %s" % (count, fileTarget))
