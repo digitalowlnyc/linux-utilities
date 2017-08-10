60dd819d3b99e52188939a52527765009d8e620d
Bryan <bryan@bluenestdigital.com>
2017-08-10 11:37:06 -0400

    Add link for append-once

--- install-linux-utils.sh
+++ install-linux-utils.sh
@@ -4,3 +4,4 @@ cp -R . /usr/local/bin/bnd
 chmod -R 775 /usr/local/bin/bnd
 
 ln -s /usr/local/bin/bnd/replace-text.sh /usr/local/bin/replacetext
+ln -s /usr/local/bin/bnd/append-once.sh /usr/local/bin/appendonce
